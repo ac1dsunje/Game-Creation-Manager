2202be2f012667cbb2bbe94775f3863437f4df94
ac1dsunje <ar.cherba63programming@gmail.com>
2026-08-25 00:09:10 +0200

    added honest coefficient

--- Assets/_Game/Scripts/Employee.cs
+++ Assets/_Game/Scripts/Employee.cs
@@ -4,13 +4,19 @@ namespace _Game.Scripts
 {
 public class Employee: MonoBehaviour
 {
+    [SerializeField] private int _honestCoefficient;
     [field: SerializeField] public Form ShownForm { get; private set; }
     [field: SerializeField] public Form RealForm { get; private set; }
     
     private void Awake()
     {
-        ShownForm = new Form(Random.Range(1, 6));
-        RealForm = new Form(Random.Range(1, 6));
+        _honestCoefficient = Random.Range(0, 10);
+        
+        ShownForm = new Form(Random.Range(1, 6), Random.Range(18, 100));
+
+        RealForm = _honestCoefficient > 5 ? 
+            ShownForm : 
+            new Form(Random.Range(1, ShownForm.Experience + 1), Random.Range(ShownForm.Age, 100));
     }
 }
 }
--- Assets/_Game/Scripts/Form.cs
+++ Assets/_Game/Scripts/Form.cs
@@ -6,11 +6,14 @@ namespace _Game.Scripts
 [Serializable]
 public class Form
 {
+    [field: SerializeField] public string Name { get; private set; } = "John Doe";
+    [field: SerializeField] public int Age { get; private set; }
     [field: SerializeField] public int Experience { get; private set; }
     
-    public Form(int experience)
+    public Form(int experience, int age)
     {
         Experience = experience;
+        Age = age;
     }
 }
 }
