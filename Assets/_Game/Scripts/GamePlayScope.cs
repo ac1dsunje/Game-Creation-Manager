312d9de5a5f7981d4c2112dcb220ade76695404b
ac1dsunje <ar.cherba63programming@gmail.com>
2026-08-24 23:20:07 +0200

    added scope

---
+++ Assets/_Game/Scripts/GamePlayScope.cs
@@ -0,0 +1,13 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace _Game.Scripts
+{
+public class GamePlayScope: LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+        builder.RegisterComponentInHierarchy<EmployeeSpawner>();
+    }
+}
+}
