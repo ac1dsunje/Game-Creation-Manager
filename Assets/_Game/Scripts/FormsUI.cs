b64aa36f4878e63708b42011b1a71846a1e76dc7
ac1dsunje <ar.cherba63programming@gmail.com>
2026-08-24 23:56:10 +0200

    added ui for forms

---
+++ Assets/_Game/Scripts/FormsUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using VContainer;
+
+namespace _Game.Scripts
+{
+public class FormsUI: MonoBehaviour
+{
+    [SerializeField] private GameObject _slot;
+    [SerializeField] private Transform _container;
+    
+    private EmployeeSpawner _spawner;
+
+    [Inject]
+    private void Construct(EmployeeSpawner spawner)
+    {
+        _spawner = spawner;
+        _spawner.OnEmployeeSpawned += CreateFormUI;
+    }
+
+    private void CreateFormUI(Employee employee)
+    {
+        var slot = Instantiate(_slot, _container).GetComponent<FormSlotUI>();
+        slot.SetEmployee(employee);
+    }
+
+    private void OnDestroy()
+    {
+        _spawner.OnEmployeeSpawned -= CreateFormUI;
+    }
+}
+}
