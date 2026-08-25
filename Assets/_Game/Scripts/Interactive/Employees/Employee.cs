448f10f6f05ef45ac0ad74616e142b821953c0b7
ac1dsunje <ar.cherba63programming@gmail.com>
2026-08-25 22:52:59 +0200

    real form priority

--- Assets/_Game/Scripts/Interactive/Employees/Employee.cs
+++ Assets/_Game/Scripts/Interactive/Employees/Employee.cs
@@ -30,11 +30,11 @@ public class Employee: InteractiveObject
     {
         _honestCoefficient = Random.Range(0, 10);
         
-        ShownForm = new Form(Random.Range(1, 6), Random.Range(18, 100));
+        RealForm = new Form(Random.Range(1, 6), Random.Range(18, 100));
 
-        RealForm = _honestCoefficient > 5 ? 
-            ShownForm : 
-            new Form(Random.Range(1, ShownForm.Efficiency + 1), Random.Range(ShownForm.Age, 100));
+        ShownForm = _honestCoefficient > 5 ? 
+            RealForm : 
+            new Form(Random.Range(RealForm.Efficiency, 6), Random.Range(RealForm.Age, 100));
     }
 
     public void Fire()
