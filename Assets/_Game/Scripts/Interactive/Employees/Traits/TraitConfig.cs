112d276a5bd2c4b07afdb8857e46a7ab89b702ca
ac1dsunje <ar.cherba63programming@gmail.com>
2026-08-29 10:27:13 +0200

    added trait sprite in config

--- Assets/_Game/Scripts/Interactive/Employees/Traits/TraitConfig.cs
+++ Assets/_Game/Scripts/Interactive/Employees/Traits/TraitConfig.cs
@@ -7,5 +7,6 @@ namespace _Game.Scripts.Interactive.Employees.Traits
 public class TraitConfig: ScriptableObject
 {
     [field: SerializeField] public EventConfig EventConfig { get; private set; }
+    [field: SerializeField] public Sprite Sprite { get; private set; }
 }
 }
